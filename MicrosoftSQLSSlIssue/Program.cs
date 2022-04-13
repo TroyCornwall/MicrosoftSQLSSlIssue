a4b00f386eedec38ed8e9ef9d7a0714d885740c1
Troy <git@troycornwall.net>
2022-04-13 16:58:10 +1200

    Initial Commit

---
+++ MicrosoftSQLSSlIssue/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+//docker build -f Dockerfile -t sqlissue . && docker run sqlissue <dbname>
+
+var server =  "test";
+if (args.Length > 0)
+{
+    server = args[0];
+}
+
+Console.WriteLine($"Connecting to {server}.database.windows.net...");
+
+string connectionString = $"Server=tcp:${server}.database.windows.net,1433;Initial Catalog=dbo;";
+
+using var connection = new SqlConnection(connectionString);
+await connection.OpenAsync();
+
+Console.WriteLine("Connected!");
